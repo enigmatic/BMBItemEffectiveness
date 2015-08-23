772c17f1a5a94947235a25e294dba649874b0718
Matt Couch <mcouch@gmail.com>
2015-08-23 16:41:13 -0500

    Initial GUI layout

--- config/env/all.js
+++ config/env/all.js
@@ -1,42 +1,45 @@
 'use strict';
 
 module.exports = {
-	app: {
-		title: 'Black Market Brawlers Item Effectiveness',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'MongoDB, Express, AngularJS, Node.js'
-	},
-	port: process.env.PORT || 3000,
-	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
-	sessionCollection: 'sessions',
-	assets: {
-		lib: {
-			css: [
+    app: {
+        title: 'Black Market Brawlers Item Effectiveness',
+        description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+        keywords: 'MongoDB, Express, AngularJS, Node.js'
+    },
+    port: process.env.PORT || 3000,
+    templateEngine: 'swig',
+    sessionSecret: 'MEAN',
+    sessionCollection: 'sessions',
+    assets: {
+        lib: {
+            css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+                'public/lib/angular-material/angular-material.css'
 			],
-			js: [
+            js: [
 				'public/lib/angular/angular.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-animate/angular-animate.js', 
+				'public/lib/angular-resource/angular-resource.js',
+				'public/lib/angular-animate/angular-animate.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-material/angular-material.js',
+                'public/lib/angular-aria/angular-aria.js'
 			]
-		},
-		css: [
+        },
+        css: [
 			'public/modules/**/css/*.css'
 		],
-		js: [
+        js: [
 			'public/config.js',
 			'public/application.js',
 			'public/modules/*/*.js',
 			'public/modules/*/*[!tests]*/*.js'
 		],
-		tests: [
+        tests: [
 			'public/lib/angular-mocks/angular-mocks.js',
 			'public/modules/*/tests/*.js'
 		]
-	}
+    }
 };
