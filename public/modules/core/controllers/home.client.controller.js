9fc264c31855e62d497bf018f9130ad51779a120
Matt Couch <mcouch@gmail.com>
2015-08-23 17:28:11 -0500

    Simple Champion Listing

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,15 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope',
-	function ($scope) {
-        // This provides Authentication context.
+angular.module('core').controller('HomeController', ['$scope', '$http',
+	function ($scope, $http) {
+        $http.get('/api/champions').success(function (response) {
+            // If successful we assign the response to the global user model
+            $scope.champions = response;
+        }).error(function (response) {
+            $scope.error = response.message;
+        });
+
 
 	}
 ]);
