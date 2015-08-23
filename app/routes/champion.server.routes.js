ceb9ff2f31423981f1982c0e9c05d8f060bd5eb7
Matt Couch <mcouch@gmail.com>
2015-08-22 22:13:12 -0500

    Add serverside champion listing

---
+++ app/routes/champion.server.routes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(app) {
+	// Routing logic
+
+	var champions = require('../../app/controllers/champions.server.controller');
+	app.route('/api/champions').get(champions.list);
+};
