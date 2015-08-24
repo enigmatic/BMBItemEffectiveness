7e64405757c63ae2b7fe26d9350fd68b8de4ac0f
Matt Couch <mcouch@gmail.com>
2015-08-24 14:17:00 -0500

    Champion selection

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -10,6 +10,15 @@ angular.module('core').controller('HomeController', ['$scope', '$http',
             $scope.error = response.message;
         });
 
+        $http.get('/api/items').success(function (response) {
+            // If successful we assign the response to the global user model
+            $scope.items = response;
+        }).error(function (response) {
+            $scope.error = response.message;
+        });
 
-	}
+        $scope.selectChampion = function ($champion) {
+            $scope.selectedChampion = $champion
+        }
+    }
 ]);
