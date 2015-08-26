984cbc812d2dc841889d1aa191c21a83e6a40266
Matt Couch <mcouch@gmail.com>
2015-08-26 15:53:06 -0500

    Fixed items per champ (wasn't actually by champ)

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -18,22 +18,51 @@ function sortItemFunction(a, b) {
 
 angular.module('core').controller('HomeController', ['$scope', '$http', '$sce',
 	function ($scope, $http, $sce) {
+        debugger;
+        $scope.items = [];
+        $scope.items.sort(sortItemFunction);
+
+        var getItems = function (champ) {
+            var scope = $scope;
+
+            if (!champ) {
+                $http.get('/api/items').success(function (response) {
+                    // If successful we assign the response to the global item model
+                    // $scope.items = response.sort(sortItemFunction);
+
+                    scope.items = response.sort(sortItemFunction);
+                    //$scope.items.addAll(response);
+
+                }).error(function (response) {
+                    scope.error = response.message;
+                });
+            } else {
+                $http.get('/api/champion/' + champ.id).success(function (response) {
+                    // If successful we assign the response to the global item model
+
+                    debugger;
+
+                    scope.items = response.sort(sortItemFunction);
+
+                    //$scope.items.addAll(response);
+
+                }).error(function (response) {
+                    scope.error = response.message;
+                });
+            }
+        };
+
         $http.get('/api/champions').success(function (response) {
-            // If successful we assign the response to the global user model
+            // If successful we assign the response to the global champion model
             $scope.champions = response;
         }).error(function (response) {
             $scope.error = response.message;
         });
 
-        $http.get('/api/items').success(function (response) {
-            // If successful we assign the response to the global user model
-            $scope.items = response.sort(sortItemFunction);
-        }).error(function (response) {
-            $scope.error = response.message;
-        });
 
         $scope.selectChampion = function ($champion) {
             $scope.selectedChampion = $champion;
+            getItems($champion);
         };
 
         $scope.TrustDangerousSnippet = function (snippet) {
@@ -44,12 +73,14 @@ angular.module('core').controller('HomeController', ['$scope', '$http', '$sce',
             $scope.selectedItem = $item;
             if ($item) {
                 $http.get('/api/item/' + $item.id).success(function (response) {
-                    // If successful we assign the response to the global user model
+                    // If successful we assign the response to the global selectItem model
                     $scope.selectedItemStats = response.sort(sortItemFunction);
                 }).error(function (response) {
                     $scope.error = response.message;
                 });
             }
         };
+
+        getItems(null);
     }
 ]);
