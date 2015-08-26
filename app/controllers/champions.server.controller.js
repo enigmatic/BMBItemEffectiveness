ee407730f2e7b46435ea75bba18706ec9ac7253f
Matt Couch <mcouch@gmail.com>
2015-08-26 15:07:15 -0500

    Add Items listing per champion to server

--- app/controllers/champions.server.controller.js
+++ app/controllers/champions.server.controller.js
@@ -15,3 +15,20 @@ exports.list = function (req, res) {
 
     res.sendFile('list.json', options);
 };
+
+exports.champion = function (req, res) {
+    var id = req.params.id;
+
+    var options = {
+        root: __dirname + '/champions/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    var filename = id + '.json';
+
+    res.sendFile(filename, options);
+};
--- app/routes/champion.server.routes.js
+++ app/routes/champion.server.routes.js
@@ -1,8 +1,9 @@
 'use strict';
 
-module.exports = function(app) {
-	// Routing logic
+module.exports = function (app) {
+    // Routing logic
 
-	var champions = require('../../app/controllers/champions.server.controller');
-	app.route('/api/champions').get(champions.list);
+    var champions = require('../../app/controllers/champions.server.controller');
+    app.route('/api/champions').get(champions.list);
+    app.route('/api/champion/:id').get(champions.champion);
 };
