fc81ee98efc32a00beeccadcfc7dbda2862f47d0
Matt Couch <mcouch@gmail.com>
2015-08-27 09:35:46 -0500

    Add route for average stats

--- app/controllers/champions.server.controller.js
+++ app/controllers/champions.server.controller.js
@@ -52,3 +52,20 @@ exports.championItem = function (req, res) {
 
     res.sendFile(filename, options);
 };
+
+exports.championStats = function (req, res) {
+    var champ = req.params.champ;
+
+    var options = {
+        root: __dirname + '/champions/items/' + champ + '/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    var filename = 'average.json';
+
+    res.sendFile(filename, options);
+};
--- app/routes/champion.server.routes.js
+++ app/routes/champion.server.routes.js
@@ -7,4 +7,5 @@ module.exports = function (app) {
     app.route('/api/champions').get(champions.list);
     app.route('/api/champion/:id').get(champions.champion);
     app.route('/api/champion/:champ/item/:item').get(champions.championItem);
+    app.route('/api/champion/:champ/stats').get(champions.championStats);
 };
